9c80f5f6c8a3daacde80eea9c4e34d0d0b677438
tomoya999 <58502947+tomoya999@users.noreply.github.com>
2022-12-20 15:08:46 +0900

    first commit

---
+++ lib/constants/operation_list.dart
@@ -0,0 +1,1 @@
+const List<String> operationList = ['÷', 'x', '-', '+'];
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'pages/calculator_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CalculatorPage(),
+    );
+  }
+}
